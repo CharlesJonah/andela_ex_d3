eadf7c7982ddee8cc9bbf3d9cbb8b6e8700c0ce8
CharlesJonah <mutua.charles48@gmail.com>
2016-12-04 09:53:23 +0300

    Update missing_number.py

--- missing_number.py
+++ missing_number.py
@@ -2,7 +2,7 @@ def find_missing(List1, List2):
   List1 = set(List1) #Convert list to set
   List2 = set(List2)  #Convert list to set
   if List1 != List2:  #compare the 2 sets
-    finallist = list(List2 - List1)
+    finallist = list(List2 - List1) #Convert the results to a list
     for i in finallist:
       return i
   else:
